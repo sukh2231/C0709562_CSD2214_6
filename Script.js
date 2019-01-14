0acf3f9c4338313030514ac5f6abb1fe0e268a8d
sukh2231 <46693375+sukh2231@users.noreply.github.com>
2019-01-14 17:15:57 -0500

    week 02

---
+++ Script.js
@@ -0,0 +1,15 @@
+// Name: Sukhraj Singh
+// Date: January 14, 2019
+// Student: C0709562
+// Course: CSD 2214 Section 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minute');
+const secondsSpan = clock.querySelector('.seconds');
+
+//console.log(endDate);
